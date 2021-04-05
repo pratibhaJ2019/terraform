dc3808744b231db3c375568648992053a9974483
pratibhaJ2019 <pratibhajadhav8@gmail.com>
2021-04-05 23:00:16 +0530

    commit

--- LaunchEc2.tf
+++ LaunchEc2.tf
@@ -12,17 +12,28 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_instance" "JenkinsServer" {
-  ami           = "ami-096fda3c22c1c990a"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = "sg-05c672069a93e2ebc"
-
-  tags = {
-    Name = "JenkinsServer"
-  }
-
 resource "aws_key_pair" "jenkinsKey" {
   key_name   = "jenkinsKey"
   public_key = file("key.pub")
 }
+
+resource "aws_instance" "JenkinsServer" {
+  ami           = "ami-096fda3c22c1c990a"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [
+    sg-05c672069a93e2ebc
+  ]
+
+  tags = {
+    Name = "JenkinsServer"
+  }
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("key")
+    host        = self.public_ip
+  }
 }
+
+
+
