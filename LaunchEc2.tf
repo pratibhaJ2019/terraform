feca30e809843803a13235d712cdc3ab3e21e994
pratibhaJ2019 <pratibhajadhav8@gmail.com>
2021-04-05 22:51:48 +0530

    commit

--- LaunchEc2.tf
+++ LaunchEc2.tf
@@ -15,8 +15,14 @@ provider "aws" {
 resource "aws_instance" "JenkinsServer" {
   ami           = "ami-096fda3c22c1c990a"
   instance_type = "t2.micro"
+  vpc_security_group_ids = "sg-05c672069a93e2ebc"
 
   tags = {
     Name = "JenkinsServer"
   }
+
+  resource "aws_key_pair" "jenkinsKey" {
+  key_name   = "jenkinsKey"
+  public_key = file("key.pub")
+}
 }
