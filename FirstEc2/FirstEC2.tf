ead39f9c2469dfbc333238eac710c6072bac923e
pratibhaJ2019 <pratibhajadhav8@gmail.com>
2021-04-10 21:51:20 +0530

    commit

--- FirstEc2/FirstEC2.tf
+++ FirstEc2/FirstEC2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "FirstEC2" {
   ami           = "ami-0742b4e673072066f"
   instance_type = "t2.micro"
   user_data = file("install_apache.sh")
-  termination_protection = true
+  disable_api_termination = true
   tags = {
     Name = "FirstEC2"
   }
