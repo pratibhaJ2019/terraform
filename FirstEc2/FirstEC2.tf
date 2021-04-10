03a56490c5bdd22f646109b83637e22d20d085fc
pratibhaJ2019 <pratibhajadhav8@gmail.com>
2021-04-10 21:46:21 +0530

    changes in userdata syntax

--- FirstEc2/FirstEC2.tf
+++ FirstEc2/FirstEC2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "FirstEC2" {
   key_name      = "newKey"
   ami           = "ami-0742b4e673072066f"
   instance_type = "t2.micro"
-  user_data = "${file("install_apache.sh")}"
+  user_data = file("install_apache.sh")
   termination_protection = true
   tags = {
     Name = "FirstEC2"
