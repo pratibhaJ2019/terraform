18c55f51236e9dc9df52bccdcf91a8e2956327d2
pratibhaJ2019 <pratibhajadhav8@gmail.com>
2021-04-10 22:09:27 +0530

    change in usedata file

--- FirstEc2/install_apache.sh
+++ FirstEc2/install_apache.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-sudo apt-get update
-sudo apt-get install -y apache2
+sudo yum update -y
+sudo yum install -y apache2
 sudo systemctl start apache2
 sudo systemctl enable apache2
 echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
-sudo mkdit tmp
+sudo mkdir tmp
