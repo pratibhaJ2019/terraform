bf55077b72a71ab808a3b83d08e103b656dc1c70
pratibhaJ2019 <pratibhajadhav8@gmail.com>
2021-04-10 22:46:27 +0530

    commit

--- FirstEc2/FirstEC2.tf
+++ FirstEc2/FirstEC2.tf
@@ -1,8 +1,12 @@
+data "template_file" "user_data" {
+template = file("install_apache.sh")
+}
+
 resource "aws_instance" "FirstEC2" {
   key_name      = "newKey"
   ami           = "ami-0742b4e673072066f"
   instance_type = "t2.micro"
-  user_data = file("install_apache.sh")
+  user_data = data.template_file.user_data.template
   disable_api_termination = true
   tags = {
     Name = "FirstEC2"
