822c94dbe9e176b352ca3cf13a06f784c3bd73f4
pratibhaJ2019 <pratibhajadhav8@gmail.com>
2021-04-10 23:05:13 +0530

    change in user data file

--- FirstEc2/install_apache.sh
+++ FirstEc2/install_apache.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 sudo yum update -y
-sudo yum install -y apache2
-sudo systemctl start apache2
-sudo systemctl enable apache2
+sudo yum install -y httpd
+sudo service httpd start  
+sudo systemctl enable httpd
 echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
 sudo mkdir tmp
