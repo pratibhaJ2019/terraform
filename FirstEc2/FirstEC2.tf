13e297213a716acf1b928f5ed88245272e3159ca
pratibhaJ2019 <pratibhajadhav8@gmail.com>
2021-04-12 15:08:40 +0530

    added cmment

--- FirstEc2/FirstEC2.tf
+++ FirstEc2/FirstEC2.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "FirstEC2" {
   ami           = "ami-0742b4e673072066f"
   instance_type = "t2.micro"
   user_data = data.template_file.user_data.template
-  disable_api_termination = true
+  #disable_api_termination = true
   tags = {
     Name = "FirstEC2"
   }
