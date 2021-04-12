703b8c668d5acd13a21a935f37c87793aec734f1
pratibhaJ2019 <pratibhajadhav8@gmail.com>
2021-04-12 17:07:27 +0530

    commit

--- FirstEc2/FirstEC2.tf
+++ FirstEc2/FirstEC2.tf
@@ -1,21 +1,16 @@
-#data "template_file" "user_data" {
-#template = file("install_apache.sh")
-#}
-
-#resource "aws_instance" "FirstEC2" {
-#  key_name      = "newKey"
-#  ami           = "ami-0742b4e673072066f"
-#  instance_type = "t2.micro"
-#  user_data = data.template_file.user_data.template
-#  #disable_api_termination = true
-#  tags = {
-#   Name = "FirstEC2"
-# }
-#}
-
+data "template_file" "user_data" {
+template = file("install_apache.sh")
+}
 
 resource "aws_instance" "FirstEC2" {
-    terraform import aws_instance.FirstEC2 i-0793f2ab9fee5d0f4
+  key_name      = "newKey"
+  ami           = "ami-0742b4e673072066f"
+  instance_type = "t2.micro"
+  user_data = data.template_file.user_data.template
+  #disable_api_termination = true
+  tags = {
+   Name = "FirstEC2"
+ }
 }
 
 resource "aws_ebs_volume" "web_storage" {
