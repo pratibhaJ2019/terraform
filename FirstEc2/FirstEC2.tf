3189ccb3f5876fae0a53f7de562c6c79c102e183
pratibhaJ2019 <pratibhajadhav8@gmail.com>
2021-04-12 16:29:46 +0530

    change in tf

--- FirstEc2/FirstEC2.tf
+++ FirstEc2/FirstEC2.tf
@@ -1,17 +1,17 @@
-data "template_file" "user_data" {
-template = file("install_apache.sh")
-}
+#data "template_file" "user_data" {
+#template = file("install_apache.sh")
+#}
 
-resource "aws_instance" "FirstEC2" {
-  key_name      = "newKey"
-  ami           = "ami-0742b4e673072066f"
-  instance_type = "t2.micro"
-  user_data = data.template_file.user_data.template
-  #disable_api_termination = true
-  tags = {
-    Name = "FirstEC2"
-  }
-}
+#resource "aws_instance" "FirstEC2" {
+#  key_name      = "newKey"
+#  ami           = "ami-0742b4e673072066f"
+#  instance_type = "t2.micro"
+#  user_data = data.template_file.user_data.template
+#  #disable_api_termination = true
+#  tags = {
+#   Name = "FirstEC2"
+# }
+#}
 resource "aws_ebs_volume" "web_storage" {
     availability_zone  = aws_instance.FirstEC2.availability_zone
     type       = "gp2"
@@ -37,6 +37,7 @@ resource "null_resource" "get_files" {
     }
     provisioner "remote-exec" {
         inline = [
+                 "sudo rm /var/www/html/index.html"
                  "sudo mkfs -t ext4 /dev/sdc",
                  "sudo mount /dev/sdc /var/www/html",
                  "sudo rm -rf /var/www/html/*",
