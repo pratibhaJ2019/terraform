55301cf09bbd562fbc5872382d804d5b07f2908b
pratibhaJ2019 <pratibhajadhav8@gmail.com>
2021-04-12 17:12:27 +0530

    commit

--- FirstEc2/install_apache.sh
+++ FirstEc2/install_apache.sh
@@ -3,5 +3,5 @@ sudo yum update -y
 sudo yum install -y httpd
 sudo service httpd start  
 sudo systemctl enable httpd
-echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+#echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
 sudo mkdir tmp
